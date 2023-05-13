510bdd1700e1e2424cd5d43da24e1b6731562925
Valkyrie-freja <yadokame3.14@gmail.com>
2023-05-14 02:23:57 +0900

    slide vertical line right to left with matrix led.

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@ const int freq_Hz = 60;
 const int time_high_ms = analog / 255.0 / freq_Hz * 1000;
 const int time_low_ms = 1.0/freq_Hz * 1000 - time_high_ms;
 
+byte vertical, horizontal;
 
 void setup() {
   shift_register.setup();
-  randomSeed(analogRead(7));
-  
+  //randomSeed(analogRead(7));
+  vertical   = B00000000;
+  horizontal = B00000001;
 }
 
 #define DT_MS 100
@@ -22,11 +24,11 @@ int num_rand;
 
 
 void loop() {
-  byte led_row = B00000011;
-  shift_register.setLedRow(led_row);
-  delay(time_high_ms);
-
-  led_row = B00000010;
-  shift_register.setLedRow(led_row);
-  delay(time_low_ms);
+  shift_register.setLedRow(vertical);
+  shift_register.setLedRow(horizontal);
+  delay(1000);
+  horizontal <<= 1;
+  if(horizontal == 0){
+    horizontal += 1;
+  }
 }
