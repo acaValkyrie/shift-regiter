a25d42781c9acc29224757ffd49b92af882f9eb7
Valkyrie-freja <yadokame3.14@gmail.com>
2023-05-15 23:48:32 +0900

    move one point to up

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,32 @@
 
 ShiftRegister shift_register = ShiftRegister( 2, 3, 4);
 
-byte vertical, horizontal;
+byte matrix_data_y, matrix_data_x;
 
 void setup() {
   shift_register.setup();
   //randomSeed(analogRead(7));
-  vertical   = B00000000;
-  horizontal = B00000001;
+  matrix_data_y = B10101010;
+  matrix_data_x = B00000001;
 }
 
+#define FPS 120.0 /*fps*/
+#define COLUMN 8.0 /*行*/
+#define DELAY_T_US (1000000.0 / FPS / COLUMN)
+
 void loop() {
-  shift_register.setLedRow(vertical);
-  shift_register.setLedRow(horizontal);
-  delay(1000);
-  horizontal <<= 1;
-  if(horizontal == 0){
-    horizontal += 1;
+  shift_register.setLedRow(matrix_data_y);
+  shift_register.setLedRow(matrix_data_x);
+
+  delayMicroseconds(DELAY_T_US);
+
+  matrix_data_y <<= 1;
+  if(matrix_data_y  == B01010100){
+    matrix_data_y += 1;
+  }
+
+  matrix_data_x <<= 1;
+  if(matrix_data_x == 0){
+    matrix_data_x += 1;
   }
 }
